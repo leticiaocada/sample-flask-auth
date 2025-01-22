8f4ed02221a279db2622482550c9505176e46f1a
Leticia Ocada dos Santos <leticiaocada@MacBook-Air-de-Leticia.local>
2025-01-21 23:37:21 -0300

    Implementing logout and login

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your secret key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -10,6 +10,12 @@ login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
 
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 @app.route('/login', methods=['POST'])
 def login():
     data = request.json
@@ -17,15 +23,18 @@ def login():
     password = data.get("password")
 
     if username and password:
-        user = User.query.filter_by(username=username)
+        user = User.query.filter_by(username=username).first()
 
         if user and user.password == password:
+            login_user(user)
             return jsonify({"message": "Successfull"})
     return jsonify({"message": "Invalid credentials"}), 400
 
-@app.route("/hello-world", methods=['GET'])
-def hello_world():
-    return "Hello world"
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "User logged out"})
 
 if __name__ == "__main__":
     app.run(debug=True)
